5046fb027ff0f206450e13cd74779245deb630a9
Gavin Garza <gaving@live.com>
2023-06-30 00:05:54 +0900

    more robust api testing

--- 1-notes/controllers/notes.js
+++ 1-notes/controllers/notes.js
@@ -6,70 +6,47 @@ notesRouter.get("/", async (req, res) => {
   res.json(notes)
 })
 
-notesRouter.get("/:id", async (req, res, next) => {
-  try {
-    const note = await Note.findById(req.params.id)
-    if (note) {
-      console.log("req.params.id", req.params.id)
-      res.json(note)
-    } else {
-      res.status(404).send({ error: "no id matches that" })
-    }
-  } catch (error) {
-    next(error)
+notesRouter.get("/:id", async (req, res) => {
+  const note = await Note.findById(req.params.id)
+  if (note) {
+    console.log("req.params.id", req.params.id)
+    res.json(note)
+  } else {
+    res.status(404).end()
   }
 })
 
-notesRouter.post("/", async (req, res, next) => {
+notesRouter.post("/", async (req, res) => {
   const body = req.body
-
-  if (body.content === undefined)
-    return res.status(400).json({ error: "content missing" })
 
   const newNote = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  try {
-    const savedNote = await newNote.save()
-    res.json(savedNote)
-  } catch (error) {
-    next(error)
-  }
+  const savedNote = await newNote.save()
+  res.status(201).json(savedNote)
 })
 
-notesRouter.delete("/:id", async (req, res, next) => {
+notesRouter.delete("/:id", async (req, res) => {
   const id = req.params.id
-  try {
-    const result = await Note.findByIdAndRemove(id)
-    if (result) {
-      res.status(204).end()
-    } else {
-      res.status(404).send({ error: "no id matches that" })
-    }
-  } catch (error) {
-    next(error)
-  }
+  await Note.findByIdAndRemove(id)
+  res.status(204).end()
 })
 
-notesRouter.put("/:id", async (req, res, next) => {
+notesRouter.put("/:id", async (req, res) => {
   const { content, important } = req.body
   const id = req.params.id
 
-  try {
-    const update = await Note.findByIdAndUpdate(
-      id,
-      { content, important },
-      { new: true, runValidators: true, context: "query" }
-    )
-    if (update) {
-      res.json(update)
-    } else {
-      res.status(404).send({ error: "no id matches that" })
-    }
-  } catch (error) {
-    next(error)
+  const update = await Note.findByIdAndUpdate(
+    id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
+  if (update) {
+    res.json(update)
+  } else {
+    res.status(404).send({ error: "no id matches that" })
   }
 })
 
