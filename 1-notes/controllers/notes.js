dc15c2925b025bace45f70c5a1970173112be71b
Gavin Garza <gaving@live.com>
2023-07-03 23:05:33 +0900

    connected notes and users collections

--- 1-notes/controllers/notes.js
+++ 1-notes/controllers/notes.js
@@ -1,9 +1,11 @@
 const notesRouter = require("express").Router()
 const Note = require("../models/note")
+const User = require("../models/user")
 
-notesRouter.get("/", async (req, res) => {
-  const notes = await Note.find({})
-  res.json(notes)
+notesRouter.get("/", async (request, response) => {
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 })
+
+  response.json(notes)
 })
 
 notesRouter.get("/:id", async (req, res) => {
@@ -18,13 +20,17 @@ notesRouter.get("/:id", async (req, res) => {
 
 notesRouter.post("/", async (req, res) => {
   const body = req.body
+  const user = await User.findById(body.userId)
 
   const newNote = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id,
   })
 
   const savedNote = await newNote.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   res.status(201).json(savedNote)
 })
 
