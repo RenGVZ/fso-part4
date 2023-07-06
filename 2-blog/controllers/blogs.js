91dd365c4867bb2578ffb99884423053819cfcf5
Gavin Garza <gaving@live.com>
2023-07-07 00:14:44 +0900

    implemented token authentication in blogApp

--- 2-blog/controllers/blogs.js
+++ 2-blog/controllers/blogs.js
@@ -1,41 +1,67 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
+const User = require("../models/user")
+const jwt = require("jsonwebtoken")
+
+const getWebToken = (request) => {
+  const authorization = request.get("authorization")
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "")
+  }
+  return null
+}
 
 blogsRouter.get("/", async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
   res.json(blogs)
 })
 
 blogsRouter.post("/", async (req, res) => {
-  const newBlog = req.body
+  const body = req.body
+
+  const decodedToken = jwt.verify(getWebToken(req), process.env.SECRET)
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: "token missing or invalid" })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  const newBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
+  }
   if (newBlog.likes === undefined) {
     newBlog.likes = 0
   }
 
   if (
-    (!newBlog.title ||
-    newBlog.title.trim().length === 0) ||
-    (!newBlog.url ||
-    newBlog.url.trim().length === 0)
+    !newBlog.title ||
+    newBlog.title.trim().length === 0 ||
+    !newBlog.url ||
+    newBlog.url.trim().length === 0
   ) {
     return res.status(400).end()
   }
 
   const blog = new Blog(newBlog)
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   res.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (req, res) => {
+blogsRouter.delete("/:id", async (req, res) => {
   const id = req.params.id
   await Blog.findByIdAndRemove(id)
   res.status(204).end()
 })
 
-blogsRouter.put('/:id', async (req, res) => {
+blogsRouter.put("/:id", async (req, res) => {
   const id = req.params.id
   const { title, author, url, likes } = req.body
-  console.log('title', title, 'author', author, 'url', url, 'likes', likes)
+  console.log("title", title, "author", author, "url", url, "likes", likes)
   const blogToUpdate = await Blog.findByIdAndUpdate(
     id,
     {
--- 2-blog/models/blog.js
+++ 2-blog/models/blog.js
@@ -3,11 +3,15 @@ const mongoose = require("mongoose")
 const blogSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   author: String,
   url: String,
-  likes: Number
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 })
 
 blogSchema.set("toJSON", {
